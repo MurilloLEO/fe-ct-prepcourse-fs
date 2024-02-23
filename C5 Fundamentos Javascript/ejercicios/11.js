4246024ce66f4bb243216f9a76ab957927156b83
Lmurillo <leobachy22@gmail.com>
2024-02-22 21:56:44 -0300

    practicando homework Js

--- C5 Fundamentos Javascript/ejercicios/11.js
+++ C5 Fundamentos Javascript/ejercicios/11.js
@@ -5,6 +5,15 @@ function esMayorDeEdad(fechaNacimiento) {
   // Devuelve true si lo es, caso contrario, retorna false.
   // PISTA: Investiga el método getFullYear() de la clase Date para obtener el año actual.
   // Tu código:
+  
+    var añoNacimiento = fechaNacimiento.getFullYear();
+    var fechaActual = new Date()
+    var añoActual = fechaActual.getFullYear()
+    var edad = añoActual - añoNacimiento
+      return edad >= 18
+  
+  
+  
 }
 
 module.exports = esMayorDeEdad;
